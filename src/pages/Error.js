d3b09b3ad1ae527bc1e37d8393d536e074a91937
Andrei Talpa <talpa.andrei@gmail.com>
2021-02-16 21:40:37 +0000

    Prettier error page with styled-components

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,11 +1,29 @@
 import React from 'react';
-
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 const Error = () => {
-  return (
-    <main>
-      <h2>Error Page</h2>
-    </main>
-  );
+  return <Wrapper>
+    <div>
+      <h1>404</h1>
+      <h3>you seem lost...</h3>
+      <Link to="/">
+        Go home
+      </Link>
+    </div>
+  </Wrapper>
 };
-
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  background: var(--clr-primary-10);
+  text-align: center;
+  h1 {
+    font-size: 10rem;
+  }
+  h3 {
+    color: var(--clr-grey-3);
+    margin-bottom: 1rem;
+  }
+`;
 export default Error;
